0b3ef240246fddb2fed2fc4825ca1ef171afbb6e
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-08-04 19:52:58 +0500

    added material ui and photos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+const useStyles=makeStyles((theme)=>({
+  root:{
+    minHeight:'100vh',
+    backgroundImage:`url(${process.env.PUBLIC_URL+'/assets/bg.jpg'})`,
+
+  },
+}));
 
 function App() {
+  const classes=useStyles();
   return (
-    <div className="App">
+    <div className={classes.root}>
     </div>
   );
 }
